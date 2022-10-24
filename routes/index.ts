d52fb313fc50f9c04d8c55f046912a3da5cb8d2e
Shahmir Noorani <shahmirn@gmail.com>
2022-10-23 20:59:45 -0500

    Support filtering by type and from.

--- routes/index.ts
+++ routes/index.ts
@@ -1,22 +1,39 @@
 import { Router } from 'express';
+import { QueryTypes } from 'sequelize';
+import sequelize from '../models/db';
 import Sms from '../models/Sms';
 import VoiceCalls from '../models/VoiceCalls';
 var router = Router();
 
 /* GET all communications */
 router.get('/', async function(req, res, next) {
-  const sms = await Sms.findAll({
-    attributes: ['datetime', 'to', 'from', 'message'],
+
+  const typeQuery = req?.query?.type as string;
+
+  const from = req.query.from as string;
+  const type = typeQuery ? typeQuery.split(',') : null;
+  
+  console.dir(req.query);
+
+  // Sequelize apparently doesn't have built-in support for Union queries, so we gotta fall back to a raw query
+  // I learned this after I'd already picked it, and time constraints mean I gotta keep going down this path
+  let query = `SELECT * FROM (SELECT datetime, "to", "from", "message", "sms" as "type" FROM 'sms' UNION SELECT datetime, "to", "from", "" as "message", "voice" as "type" FROM 'voice_calls')  where 1=1`;
+  if (from) {
+    query += ` AND "from" = :from`;
+  }
+  if (type) {
+    query += ` AND type IN (:type)`;
+  }
+
+  const conversations = await sequelize.query(query,
+  {
+    replacements: { from, type },
+    type: QueryTypes.SELECT
   });
 
-  const voiceCalls = await VoiceCalls.findAll({
-    attributes: ['datetime', 'to', 'from'],
-  });
+  console.log(conversations.length);
 
-  res.json([
-    ...sms,
-    ...voiceCalls,
-  ])
+  res.json(conversations);
 });
 
 export default router;
